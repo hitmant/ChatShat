2af5e59a38eeb2e6e7caea554d817244fa5f359c
hitmant <nathandrake@windowslive.com>
2017-10-15 20:54:45 +0300

    Testing out Git.

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
+/*This file is just to figure out how
+ * to use git within windows, since
+ * I've forgot.
+ */
 public class Hello{
+
 	public static void main(String[] a){
 		System.out.println("Hello world!");
 	}
+
 }
