8888e9ae3102db88d6a570a7b669d859c4f35ddc
hitmant <nathandrake@windowslive.com>
2017-10-16 11:41:58 +0300

    Got laptop working!

--- src/Hello.java
+++ src/Hello.java
@@ -1,11 +1,15 @@
 /*This file is just to figure out how
  * to use git within windows, since
  * I've forgot.
+ *
+ * Seems like I've managed to get
+ * git working!
  */
 public class Hello{
 
 	public static void main(String[] a){
 		System.out.println("Hello world!");
+		System.out.println("This was written on my laptop");
 	}
 
 }
